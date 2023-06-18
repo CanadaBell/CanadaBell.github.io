cb82cd5daf3f31823a93e7903c0692f8b98173c0
CanadaBell <123008017+CanadaBell@users.noreply.github.com>
2023-06-17 18:55:15 -0600

    Re-wrote js for line from 2 points
    - Added an event listener that checks for all inputs not just a specific one
    - Added comments
    - Changed other small things

--- Other Pages/Linear Equations/2pt_line.js
+++ Other Pages/Linear Equations/2pt_line.js
@@ -1,132 +1,105 @@
-// Declare the needed variables 
+// *** Variables ***
+
+// ** Line **
+
+// * Point 1 *
 let x1 = 300
 let y1 = 300
+
+// * Point 2 *
 let x2 = 300
 let y2 = 300
 
-// canvas w and H
-const canvasW = 600
-const canvasH = 600
+// ** Canvas **
+const canvasWidth = 600
+const canvasHeight = 600
 
-// Clears the inputs after each page reloads
-window.onload = function() {
+
+
+// Clears input each time page loads
+document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('point1x').value = ''
   document.getElementById('point1y').value = ''
   document.getElementById('point2x').value = ''
   document.getElementById('point2y').value = ''
-}
+})
 
-// Inputs
-document.addEventListener('DOMContentLoaded', function() {
+// *** Reading Inputs ***
+document.addEventListener('DOMContentLoaded', () => {
 
-  document.getElementById("point1x").addEventListener('input', () => {
-    x1 = document.getElementById('point1x').value // x1 becomes what the value of the input is
-    if (!x1) { x1 = 0 }// If x1 is an empty string x1 becomes 0
+  const allInputs = document.querySelectorAll('input'); // * Variable that creates a list of all inputs  
+  allInputs.forEach(input => { // * Itterates between all inputs *
+    input.addEventListener('input', () => { // * Checks for update
+      x1 = document.getElementById('point1x').value; y1 = document.getElementById('point1y').value // * Value of left inputs *
+      x2 = document.getElementById('point2x').value; y2 = document.getElementById('point2y').value // Value of right inputs
 
-    testForstring: if (isNaN(x1)) {
+      if (!x1) { x1 = 0 }; if (!y1) { y1 = 0 }; if (!x2) { x2 = 0 }; if (!y2) { y2 = 0 } // * Any empty inputs gets assigned the value 0
 
-      if (x1 === '-') { break testForstring }
 
-      document.getElementById('point1x').value = ''
-    } // If x1 isn't a number or a dash, remove it
+      // FOR ALL INPUTS //
+      // * Checks if inputed value is an NaN (Not a number), if yes BUT it is a - then programs skips the rest of the code, otherwise the input becomes 0 along with the variable to go with the input
+      if (isNaN(x1)) {
+        if (x1 === "-") { return }
+        document.getElementById("point1x").value = ''
+        x1 = 0
+      }
+      if (isNaN(y1)) {
+        if (y1 === "-") { return }
+        document.getElementById("point1y").value = ''
+        y1 = 0
+      }
 
-    if (x1 > 25) { 
-      document.getElementById('point1x').value = 25 
-      x1 = 25
-    }// If x1 is bigger than 25, make x1 25
+      if (isNaN(x2)) {
+        if (x2 === "-") { return }
+        document.getElementById("point2x").value = ''
+        x2 = 0
+      }
+      if (isNaN(y2)) {
+        if (y2 === "-") { return }
+        document.getElementById("point2y").value = ''
+        y2 = 0
+      }
 
-    if (x1 < -25) { 
-      document.getElementById('point1x').value = -25 
-      x1 = -25
-    }// If x1 is smaller than 25, make x1 -25
+      x1 *= 1; y1 *= 1; y1 *= 1; y2 *= 1 // * Turns each string to int *
 
-    x1 = (canvasW / 2) + (x1 * 12) // multiply x1 by 12 and add it to half the canvas width
-  })
 
-  document.getElementById("point1y").addEventListener('input', () => {
-    y1 = document.getElementById('point1y').value // y1 becomes what the value of the input is
-    if (!y1) { y1 = 0 }// If y1 is an empty string y1 becomes 0
+      // FOR ALL INPUTS //
+      // * Checks if inputed value is less than -25 or greater than 25, if yes, it check whether or not the number is negative or positive and changes the number
+      if (x1 < -25 || x1 > 25) {
+        overOrUnder =  (x1 <-25 ? -25 : 25)
+        document.getElementById("point1x").value = overOrUnder
+        x1 = overOrUnder
+      }
+      if (y1 < -25 || y1 > 25) { 
+        overOrUnder = (y1 <-25 ? -25 : 25)
+        document.getElementById("point1y").value = overOrUnder
+        y1 = overOrUnder
+      }
+      if (x2 < -25 || x2 > 25) { 
+        overOrUnder = (x2 <-25 ? -25 : 25)
+        document.getElementById("point2x").value = overOrUnder
+        x2 = overOrUnder
+      }
+      if (y2 < -25 || y2 > 25) {
+        overOrUnder = (y2 <-25 ? -25 : 25)
+        document.getElementById("point2y").value = overOrUnder
+        y2 = overOrUnder
+      }
 
-    testForstring: if (isNaN(y1)) {
+      // * After all the checks it turns the inputed value into a useable number, by multiplying all numbers by 12 and adding half the canvas width to the x values and subtracting half the height from the y values *
+      x1 = (canvasWidth / 2) + (x1 * 12); y1 = (canvasHeight / 2) - (y1 * 12) 
+      x2 = (canvasWidth / 2) + (x2 * 12); y2 = (canvasHeight / 2) - (y2 * 12) 
 
-      if (y1 === '-') { break testForstring }
-  
-      document.getElementById('point1y').value = ''
-    } // If x1 isn't a number or a dash, remove it
+    });
 
-    if (y1 > 25) { 
-      document.getElementById('point1y').value = 25 
-      y1 = 25 
-    }// If y1 is bigger than 25, make y1 25
-
-    if (y1 < -25) { 
-      document.getElementById('point1y').value = -25 
-      y1 = -25
-    }// If y1 is smaller than 25, make y1 -25
-
-    y1 = (canvasH / 2) - (y1 * 12) // multiply y1 by 12 and subtract it to half the canvas height
-  })
-
-  document.getElementById("point2x").addEventListener('input', () => {
-    x2 = document.getElementById('point2x').value // x2 becomes what the value of the input is
-    if (!x2) { x2 = 0 }// If x2 is an empty string x2 becomes 0
-
-    // If x2 isn't a number or a dash, remove it
-    testForstring: if (isNaN(x2)) { 
-
-      if (x2 === '-') { break testForstring }
-
-      document.getElementById('point2x').value = ''
-
-    } 
-
-    // If x2 is bigger than 25, make x2 25
-    if (x2 > 25) { 
-      document.getElementById('point2x').value = 25 
-      x2 = 25
-    }
-
-    // If x2 is smaller than 25, make x2 -25
-    if (x2 < -25) { 
-      document.getElementById('point2x').value = -25
-      x2 = -25 
-    }
-    x2 = (canvasW / 2) + (x2 * 12) // multiply x2 by 12 and add it to half the canvas width
-  })
-
-  document.getElementById("point2y").addEventListener('input', () => {
-    y2 = document.getElementById('point2y').value // y2 becomes what the value of the input is
-    if (!y2) { y2 = 0 }// If y2 is an empty string y2 becomes 0
-
-    // If x2 isn't a number or a dash, remove it
-    testForstring: if (isNaN(y2)) {
-
-      if (y2 === '-') { break testForstring }
-
-      document.getElementById('point2y').value = ''
-
-    }
-
-    // If y2 is bigger than 25, make y2 25
-    if (y2 > 25) {
-      document.getElementById('point2y').value = 25 
-      y2 = 25
-    }
-
-    // If y2 is smaller than 25, make y2 -25
-    if (y2 < -25) { 
-      document.getElementById('point2y').value = -25 
-      y2 = -25
-    }
-
-    y2 = (canvasH / 2) - (y2 * 12) // multiply y2 by 12 and subtract it to half the canvas height
-  })
+  });
 
 });
+// *** Drawing ***
 
-function setup() {
-  var canvas = createCanvas(canvasW, canvasH);
-  canvas.parent('sketch-holder');
+function setup() { 
+  var canvas = createCanvas(canvasWidth, canvasHeight); // * ceating a variable with the data for a canvas*
+  canvas.parent('sketch-holder'); // * Set the parent to the canvas be the div *
 }
 
 function draw() {
@@ -136,19 +109,19 @@ function draw() {
   stroke(255, 0, 255)
   strokeWeight(3)
 
-  for (let x = 12 ; x < canvasW; x+=12) {
-    line (x, (canvasW / 2) - 3, x, (canvasW / 2) + 3) // Loop that creates dashes for horizontal line
+  for (let x = 12 ; x < canvasWidth; x+=12) {
+    line (x, (canvasWidth / 2) - 3, x, (canvasWidth / 2) + 3) // Loop that creates dashes for horizontal line
   }
 
-  for (let x = 12 ; x < canvasH; x+=12) {
-    line((canvasH / 2) - 3, x, (canvasH / 2) + 3, x) // Loop that creates dashes for vertical line
+  for (let x = 12 ; x < canvasHeight; x+=12) {
+    line((canvasHeight / 2) - 3, x, (canvasHeight / 2) + 3, x) // Loop that creates dashes for vertical line
   }
 
   stroke(176, 0, 230)
   strokeWeight(4)
 
-  line(canvasH / 2, 0, canvasH / 2, canvasH) // Vertical Line 
-  line(0, canvasW / 2, canvasW, canvasW / 2) // Horizontal Line
+  line(canvasHeight / 2, 0, canvasHeight / 2, canvasHeight) // Vertical Line 
+  line(0, canvasWidth / 2, canvasWidth, canvasWidth / 2) // Horizontal Line
 
   // Drawing the line from 2 points
   stroke(0, 0, 255)
@@ -161,5 +134,4 @@ function draw() {
 
   stroke(255, 255, 0)
   point(x2, y2)
-  console.log(x1, y1, x2, y2)
 }
