9fe458a58a736f1144618876b1767d490e282120
CanadaBell <123008017+CanadaBell@users.noreply.github.com>
2023-10-05 09:16:21 -0600

    Add files via upload

--- Other Pages/Linear Equations/2pt_line.js
+++ Other Pages/Linear Equations/2pt_line.js
@@ -1,43 +1,57 @@
-// *** Variables ***
+// Variables
 
-// ** Line **
+var canvasWidth = 600 
+var canvasHeight = 600
+// Base canvas dimentions
 
-// * Point 1 *
-let x1 = 300
-let y1 = 300
+const canvasChangeAmount = (600 /(Math.round(screen.width / 50) * 50)).toFixed(2)
+// divides 600 by the width of the user's screen -> rounds number to nearest hundreth (0.0?)
 
-// * Point 2 *
-let x2 = 300
-let y2 = 300
+if (screen.width < 576) { // Checks if user is on phone
+  canvasWidth = Math.floor(canvasWidth / canvasChangeAmount) 
+  canvasHeight = Math.floor(canvasHeight / canvasChangeAmount)
+  // canvas dimentions change depending on the size of the phone screen
+}
 
-// ** Canvas **
-const canvasWidth = 600
-const canvasHeight = 600
+const halfOfCanvasW = canvasWidth / 2
+const halfOfCanvasH = canvasHeight / 2
+// gets what half of the canvas width / height 
 
 
+let x1 = halfOfCanvasW, x2 = halfOfCanvasW
+let y1 = halfOfCanvasH, y2 = halfOfCanvasH
+// sets x1 and x2 to be half of the canvas width | sets y1 and y2 to be half of the canvas width
 
-// Clears input each time page loads
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('point1x').value = ''
+
+document.addEventListener('DOMContentLoaded', function() { // Clears input each time page loads
   document.getElementById('point1y').value = ''
   document.getElementById('point2x').value = ''
   document.getElementById('point2y').value = ''
 })
 
-// *** Reading Inputs ***
-document.addEventListener('DOMContentLoaded', () => {
 
-  const allInputs = document.querySelectorAll('input'); // * Variable that creates a list of all inputs  
-  allInputs.forEach(input => { // * Itterates between all inputs *
-    input.addEventListener('input', () => { // * Checks for update
-      x1 = document.getElementById('point1x').value; y1 = document.getElementById('point1y').value // * Value of left inputs *
-      x2 = document.getElementById('point2x').value; y2 = document.getElementById('point2y').value // Value of right inputs
+document.addEventListener('DOMContentLoaded', () => { // Code that reads inputs
+  
+  const allInputs = document.querySelectorAll('input'); // Variable that creates a list of all inputs  
+  allInputs.forEach(input => { // Itterates between all inputs 
+    input.addEventListener('input', () => { // Checks for update
+      x1 = document.getElementById('point1x').value 
+      y1 = document.getElementById('point1y').value 
+      // Value of left inputs 
 
-      if (!x1) { x1 = 0 }; if (!y1) { y1 = 0 }; if (!x2) { x2 = 0 }; if (!y2) { y2 = 0 } // * Any empty inputs gets assigned the value 0
+      x2 = document.getElementById('point2x').value
+      y2 = document.getElementById('point2y').value 
+      // Value of right inputs
+
+      if (!x1) { x1 = 0 } 
+      if (!y1) { y1 = 0 }
+      if (!x2) { x2 = 0 }
+      if (!y2) { y2 = 0 } 
+      // Any empty inputs gets assigned the value 0
 
 
-      // FOR ALL INPUTS //
-      // * Checks if inputed value is an NaN (Not a number), if yes BUT it is a - then programs skips the rest of the code, otherwise the input becomes 0 along with the variable to go with the input
+  
+      // Checks if inputed value is an NaN (Not a number), if yes BUT it is a - then programs skips the rest of the code, otherwise the input becomes 0 along with the variable to go with the input (same for all)
       if (isNaN(x1)) {
         if (x1 === "-") { return }
         document.getElementById("point1x").value = ''
@@ -60,11 +74,11 @@ document.addEventListener('DOMContentLoaded', () => {
         y2 = 0
       }
 
-      x1 *= 1; y1 *= 1; y1 *= 1; y2 *= 1 // * Turns each string to int *
+      x1 *= 1; y1 *= 1; y1 *= 1; y2 *= 1 
+      // Turns each string to int 
 
 
-      // FOR ALL INPUTS //
-      // * Checks if inputed value is less than -25 or greater than 25, if yes, it check whether or not the number is negative or positive and changes the number
+      // Checks if inputed value is less than -25 or greater than 25, if yes, it check whether or not the number is negative or positive and changes the number (for all inputs)
       if (x1 < -25 || x1 > 25) {
         overOrUnder =  (x1 <-25 ? -25 : 25)
         document.getElementById("point1x").value = overOrUnder
@@ -86,52 +100,69 @@ document.addEventListener('DOMContentLoaded', () => {
         y2 = overOrUnder
       }
 
-      // * After all the checks it turns the inputed value into a useable number, by multiplying all numbers by 12 and adding half the canvas width to the x values and subtracting half the height from the y values *
-      x1 = (canvasWidth / 2) + (x1 * 12); y1 = (canvasHeight / 2) - (y1 * 12) 
-      x2 = (canvasWidth / 2) + (x2 * 12); y2 = (canvasHeight / 2) - (y2 * 12) 
+      // takes small number (positive or negative) and turns it into a positive number that p5 can use as points on a plane where the orgin is top left
+      // anything less than half of the dimentions is negative, while anything more is positive
+      x1 = (halfOfCanvasW) + (x1 * (halfOfCanvasW / 25))
+      y1 = (halfOfCanvasH) - (y1 * (halfOfCanvasH / 25))
+
+      x2 = (halfOfCanvasW) + (x2 * (halfOfCanvasW / 25))
+      y2 = (halfOfCanvasH) - (y2 * (halfOfCanvasH / 25)) 
 
     });
 
   });
 
 });
-// *** Drawing ***
 
+// Actual code for drawing some lines
 function setup() { 
-  var canvas = createCanvas(canvasWidth, canvasHeight); // * ceating a variable with the data for a canvas*
-  canvas.parent('sketch-holder'); // * Set the parent to the canvas be the div *
+  var canvas = createCanvas(canvasWidth, canvasHeight); // creates the canvas itself
+  canvas.parent('sketch-holder'); // makes it be able to have css
 }
 
 function draw() {
-  // Creating The Lines on the graph
-  background(31)
 
-  stroke(255, 0, 255)
-  strokeWeight(3)
+  background(31) // sets background to be rgb(31, 31, 31)
 
-  for (let x = 12 ; x < canvasWidth; x+=12) {
-    line (x, (canvasWidth / 2) - 3, x, (canvasWidth / 2) + 3) // Loop that creates dashes for horizontal line
+  stroke(255, 0, 255) // sets line to be rgb(255, 0, 255)
+  strokeWeight(3) // line thickness of 3
+
+  for (let x = halfOfCanvasW / 25; x < canvasWidth; x+= halfOfCanvasW / 25) {
+    line (x, (halfOfCanvasW) - 3, x, (halfOfCanvasW) + 3) // Loop that creates dashes for horizontal line
   }
 
-  for (let x = 12 ; x < canvasHeight; x+=12) {
-    line((canvasHeight / 2) - 3, x, (canvasHeight / 2) + 3, x) // Loop that creates dashes for vertical line
+  for (let x = halfOfCanvasH / 25; x < canvasHeight; x+= halfOfCanvasH / 25) {
+    line((halfOfCanvasH) - 3, x, (halfOfCanvasH) + 3, x) // Loop that creates dashes for vertical line
   }
 
   stroke(176, 0, 230)
   strokeWeight(4)
 
-  line(canvasHeight / 2, 0, canvasHeight / 2, canvasHeight) // Vertical Line 
-  line(0, canvasWidth / 2, canvasWidth, canvasWidth / 2) // Horizontal Line
+  line(halfOfCanvasH, 0, halfOfCanvasH, canvasHeight) // Vertical Line 
+  line(0, halfOfCanvasW, canvasWidth, halfOfCanvasW) // Horizontal Line
 
   // Drawing the line from 2 points
   stroke(0, 0, 255)
   line(x1, y1, x2, y2)
 
-  strokeWeight(7)
+  if (screen.width < 576) {
+    strokeWeight(3.5)
+  } else {
+    strokeWeight(7)
+  }
+  
 
   stroke(0, 255, 0)
   point(x1, y1)
 
   stroke(255, 255, 0)
   point(x2, y2)
+}
+
+function windowResized() {
+  if (screen.width < 567) {
+    resizeCanvas(canvasWidth, canvasHeight)
+  } else {
+    resizeCanvas(600, 600)
+  }
 }
